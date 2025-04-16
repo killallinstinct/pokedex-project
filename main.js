aa719049698c999f266c5a3558e5a285764877be
killallinstinct <aedwa054@odu.edu>
2025-04-16 16:09:39 -0400

    Added color backgrounds for pokemon cards based on primary type and a visual for stats

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { calculateBST, formatAbilities } from "./utils.js";
+import { typeColors } from "./typeColors.js";
 
 fetch("pokemon.json")
     .then((res) => res.json())
@@ -12,17 +13,28 @@ fetch("pokemon.json")
             // Display bool for the abilities on the object
             const abilityHTML = formatAbilities(pokemon.abilities);
 
+            // Adds background color to pokemon card based 
+            // on their primary type
+            const primaryType = pokemon.types[0];
+            const bgColor = typeColors[primaryType] || "#777"   // fallback
+            card.style.backgroundColor = bgColor;
+
             // Displays pokemon data in form of a card
             const card = document.createElement("div");
             card.className = "pokemon-card";
             card.innerHTML = `
             <img src="${pokemon.sprite}" alt="${pokemon.name}" />
-            <h2>${pokemon.name} (${pokemon.num})</h2
+            <h2>${pokemon.name} (${pokemon.num})</h2>
             <p>Type: ${pokemon.types.join(', ')}</p>
             ${abilityHTML}
             <p>Height: ${pokemon.heightm} m</p>
             <p>Weight: ${pokemon.weightkg} kg</p>
-            <p>HP: ${pokemon.baseStats.hp}</p>
+            <p>HP: ${pokemon.baseStats.hp}
+                <div class="stat-bar-container>
+                    <div class="stat-bar" style="width: 
+                    ${(pokemon.baseStats.hp / 255) * 100}%;"><\div>
+                </div>
+            </p>
             <p>Attack: ${pokemon.baseStats.atk}</p>
             <p>Defense: ${pokemon.baseStats.def}</p>
             <p>Sp. Attack: ${pokemon.baseStats.spa}</p>
---
+++ typeColors.js
@@ -0,0 +1,21 @@
+export const typeColors = {
+    Fire: "#F08030",
+    Water: "#6890F0",
+    Grass: "#78C850",
+    Electric: "#F8D030",
+    Ice: "#98D8D8",
+    Flying: "#A890F0",
+    Fairy: "#EE99AC",
+    Poison: "#A040A0",
+    Bug: "#A8B820",
+    Normal: "#A8A878",
+    Ground: "#E0C068",
+    Fighting: "#C03028",
+    Psychic: "#F85888",
+    Rock: "#B8A038",
+    Ghost: "#705898",
+    Dragon: "#7038F8",
+    Dark: "#705848",
+    Steel: "#B8B8D0"
+  };
+  
